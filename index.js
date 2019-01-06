c670a05b87354776f711dba26d64f11c8609d7dc
Rick Pearce <rpearce63@yahoo.com>
2019-01-06 11:46:46 -0600

    add cors

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const app = express();
+const router = require("./router");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
+// for mocking the actual mongodb connection
 const Mockgoose = require("mockgoose").Mockgoose;
 const mockgoose = new Mockgoose(mongoose);
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const app = express();
-//const os = require("os");
-//console.log("temp dir: ", os.tmpdir);
-app.use(morgan("dev")); // log every request to the console
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: "true" })); // parse application/x-www-form-urlencoded
-app.use(bodyParser.json()); // parse application/json
-app.use(bodyParser.json({ type: "application/vnd.api+json" })); // parse application/vnd.api+json as json
 
-require("./router")(app);
 mockgoose.helper.setDbVersion("3.2.1");
 mockgoose.prepareStorage().then(function() {
   console.log("setting up mockgoose connection");
@@ -31,8 +27,17 @@ mockgoose.prepareStorage().then(function() {
       console.log(err);
       throw err;
     });
-  app.listen("3090", function(err) {
-    if (err) throw err;
-    console.log("App listening on port " + "3090");
-  });
 });
+
+// App setup
+app.use(morgan("combined")); // log every request to the console
+app.use(cors());
+//app.use(bodyParser.urlencoded({ extended: "true" })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json({ type: "*/*" })); // parse application/vnd.api+json as json
+router(app);
+
+// Server setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log("App listening on port " + port);
